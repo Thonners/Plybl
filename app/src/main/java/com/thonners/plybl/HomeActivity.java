44186771f78e48f7aaae24e624e897759361ea8b
Mathonwy Thomas <mathonwy.thomas@gmail.com>
2017-05-17 23:12:10 +0100

    Create HomeActivity and set up header portion of layout

--- app/src/main/java/com/thonners/plybl/HomeActivity.java
+++ app/src/main/java/com/thonners/plybl/HomeActivity.java
@@ -3,6 +3,7 @@ package com.thonners.plybl;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener{
+
+    private final String LOG_TAG = "HomeActivity" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +27,25 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // FABs
+        FloatingActionButton fabChairs  = (FloatingActionButton) findViewById(R.id.fab_chairs) ;
+        FloatingActionButton fabShelves = (FloatingActionButton) findViewById(R.id.fab_shelves) ;
+        FloatingActionButton fabTables  = (FloatingActionButton) findViewById(R.id.fab_tables) ;
+        FloatingActionButton fabGarden  = (FloatingActionButton) findViewById(R.id.fab_garden) ;
+        // OnClickListeners
+        fabChairs.setOnClickListener(this);
+        fabShelves.setOnClickListener(this);
+        fabTables.setOnClickListener(this);
+        fabGarden.setOnClickListener(this);
     }
 
     @Override
@@ -97,5 +103,36 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * Handle clicks
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.fab_chairs:
+                showComingSoonToast("chairs");
+                break;
+            case R.id.fab_shelves:
+                Log.d(LOG_TAG,"fab_shelves clicked") ;
+                break;
+            case R.id.fab_tables:
+                showComingSoonToast("tables");
+                break;
+            case R.id.fab_garden:
+                showComingSoonToast("outdoor things");
+                break;
+        }
+    }
+
+    /**
+     * Shows a toast saying 'this isn't yet available'
+     * @param whatsComingSoon The 'this' that isn't available
+     */
+    private void showComingSoonToast(String whatsComingSoon) {
+        String message = "Sorry, " + whatsComingSoon + " are not yet available. Please try shelves!";
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
